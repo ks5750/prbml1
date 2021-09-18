975edbdf1b9cc58f14ef8ccf0b24e25ea70c8198
kumar <kumar.saswat@gmail.com>
2021-09-18 00:55:55 -0400

    Final commit.

--- solution.py
+++ solution.py
@@ -5,8 +5,10 @@ from nacl.exceptions import CryptoError
 import sys
 import json
 
-with open(sys.argv[1]) as json_data:
-    inputs = json.load(json_data)
+## with open(sys.argv[1]) as json_data:
+##    inputs = json.load(json_data)
+##
+inputs = json.load(sys.stdin)
 
 outputs = {}
 
@@ -28,6 +30,39 @@ output_byte=bytes.fromhex(input_hexstr)
 
 output_str=output_byte.decode()
 outputs["problem2"]= output_str
+
+# Problem 3
+
+input_str=inputs["problem3"]
+output_bytes=input_str.encode()
+
+output_hex=output_bytes.hex()
+outputs["problem3"]= output_hex
+
+# Problem 4
+
+in_cyper_hex=inputs["problem4"]
+ciphertext_bytes = bytes.fromhex(in_cyper_hex)
+key = b"A" * 32
+nonce = b"B" * 24
+plaintext_bytes = SecretBox(key).decrypt(ciphertext_bytes, nonce)
+plaintext_string = plaintext_bytes.decode()
+outputs["problem4"] = plaintext_string
+
+# Problem 5
+ciphertext_list = inputs["problem5"]
+key = b"C" * 32
+nonce = b"D" * 24
+for ciphertext_hex in ciphertext_list:
+    ciphertext_bytes = bytes.fromhex(ciphertext_hex)
+    try:
+        plaintext_bytes = SecretBox(key).decrypt(ciphertext_bytes, nonce)
+    except CryptoError:
+        # Bad ciphertext
+        continue
+    plaintext_string = plaintext_bytes.decode()
+    outputs["problem5"] = plaintext_string
+    break
 
 
 # Output
